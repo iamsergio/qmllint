f686df0d196420a641d0f43cb0a605d1551a6b9e
Sergio Martins <iamsergio@gmail.com>
2014-10-06 16:20:57 +0100

    Sync with upstream.
    Uses QCommandLineParser and QCoreApplication now.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
-#include <QGuiApplication>
+/****************************************************************************
+**
+** Copyright (C) 2014 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Sergio Martins <sergio.martins@kdab.com>
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL21$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia. For licensing terms and
+** conditions see http://qt.digia.com/licensing. For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights. These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include <QDebug>
 #include <QFile>
-#include <QStringList>
 #include <QFileInfo>
-#include <QTextStream>
+#include <QCommandLineParser>
+#include <QCoreApplication>
 
 #include <QtQml/private/qqmljslexer_p.h>
 #include <QtQml/private/qqmljsparser_p.h>
@@ -64,22 +96,28 @@ static bool lint_file(const QString &filename, bool silent)
 
 int main(int argv, char *argc[])
 {
-    QGuiApplication app(argv, argc);
+    QCoreApplication app(argv, argc);
+    QCoreApplication::setApplicationName("qmllint");
+    QCoreApplication::setApplicationVersion("1.0");
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QLatin1String("QML syntax verifier"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption silentOption(QStringList() << "s" << "silent", QLatin1String("Don't output syntax errors"));
+    parser.addOption(silentOption);
+    parser.addPositionalArgument(QLatin1String("files"), QLatin1String("list of qml or js files to verify"));
 
-    if (argv < 2) {
-        QTextStream stream(stdout);
-        stream << QObject::tr("Usage: qmllint [--silent] <file1> ... <fileN>") << "\n\n"
-               << QObject::tr("Where <file> is either a .js or a .qml file.") << "\n";
-        return -2;
+    parser.process(app);
+
+    if (parser.positionalArguments().isEmpty()) {
+        parser.showHelp(-1);
     }
 
-    QStringList arguments = app.arguments();
-    arguments.removeFirst();
-    bool silent = arguments.removeAll(QString("--silent")) > 0;
+    bool silent = parser.isSet(silentOption);
     bool success = true;
-    foreach (const QString &filename, arguments) {
+    foreach (const QString &filename, parser.positionalArguments()) {
         success &= lint_file(filename, silent);
     }
-    
+
     return success ? 0 : -1;
 }
